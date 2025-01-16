3599220f762ad9692854195c753d8a3c207b61fc
Governor Narh <ron468896@gmail.com>
2025-01-16 17:50:53 +0000

    Start connections

--- src/pages/ConnectionsPassenger.jsx
+++ src/pages/ConnectionsPassenger.jsx
@@ -3,14 +3,26 @@ import { Container, Grid, Typography, Box } from "@mui/material";
 import { useAuth } from "../helpers/GeneralContext";
 import { LanguageContext } from "../helpers/LanguageContext";
 import ConnectionCard from "../components/ConnectionCard";
+import useCurrentUserDoc from "../hooks/currentUserDoc";
 
 const ConnectionsPassenger = () => {
-  const { connections, currentUser } = useAuth();
+  // const { connections, currentUser } = useAuth();
   const { language } = useContext(LanguageContext);
 
-  const passengerConnections = connections.filter(
-    (connection) => currentUser.accountType === "Passenger"
-  );
+  // const passengerConnections = connections.filter(
+  //   (connection) => currentUser.accountType === "Passenger"
+  // );
+
+  const {
+    currentUserDoc: currentUser,
+    currentUserDocLoading,
+    refreshCurrentUserDoc,
+    rideData,
+    rideDataLoading,
+    refreshRideData,
+  } = useCurrentUserDoc();
+
+  console.log("Ride data-->", rideData);
 
   return (
     <Container>
@@ -24,7 +36,7 @@ const ConnectionsPassenger = () => {
       >
         {language.connectionsPassenger.title}
       </h1>
-      {passengerConnections.length === 0 ? (
+      {/* {passengerConnections.length === 0 ? (
         <Box sx={{ textAlign: "center", marginTop: 4 }}>
           <img
             src="/empty.png"
@@ -44,6 +56,45 @@ const ConnectionsPassenger = () => {
             </Grid>
           ))}
         </Grid>
+      )} */}
+      {rideDataLoading ? (
+        <Typography sx={{ opacity: 0.6, marginTop: 1 }}>Loading...</Typography>
+      ) : !rideData ? (
+        <Box sx={{ textAlign: "center", marginTop: 4 }}>
+          <img
+            src="/empty.png"
+            width="150px"
+            style={{ marginTop: "40px" }}
+            alt={language.connectionsPassenger.noConnectionsAlt}
+          />
+          <Typography sx={{ opacity: 0.6, marginTop: 1 }}>
+            {language.connectionsPassenger.noConnectionsMessage}
+          </Typography>
+        </Box>
+      ) : (
+        <Box>
+          <Typography sx={{ opacity: 0.8, marginTop: 1 }}>
+            {rideData.car.name}
+          </Typography>
+          <Typography sx={{ opacity: 0.8, marginTop: 1 }}>
+            {rideData.car.plate}
+          </Typography>
+          <Box display="flex" flexWrap="wrap">
+            <Typography sx={{ opacity: 0.8, marginTop: 1 }}>
+              Stop Points:{" "}
+            </Typography>
+            {rideData.stopPoints.map((point, index) => {
+              return (
+                <Box display="flex" flexWrap="wrap">
+                  <Typography sx={{ opacity: 0.6, marginTop: 1 }}>
+                    {point}
+                  </Typography>
+                  ,{" "}
+                </Box>
+              );
+            })}
+          </Box>
+        </Box>
       )}
       <div style={{ height: "16px" }} />
     </Container>
